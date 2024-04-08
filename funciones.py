05b4f905e6527b8427c856ba77f26a12b9c2c532
SaraDiaz2001 <sara.diaz1@udea.edu.co>
2024-04-08 15:13:21 -0500

    Intento 1 : Actualizar repositorio ignorando el entorno virtual

---
+++ funciones.py
@@ -0,0 +1,12 @@
+def sum(num_1: float,num_2: float) -> float:
+    """_summary_
+
+    Args:
+        num_1 (float): _description_
+        num_2 (float): _description_
+
+    Returns:
+        float: _description_
+    """
+    valor_suma: float = num_1 + num_2
+    return valor_suma
