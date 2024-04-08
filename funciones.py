ebc15546b0bb3c2165af9e83af6ae08dfac4c4b7
SaraDiaz2001 <sara.diaz1@udea.edu.co>
2024-04-08 16:24:17 -0500

    Ejercicios de funciones en python

--- funciones.py
+++ funciones.py
@@ -1,3 +1,4 @@
+#EJERCICIO DE CLASE 
 def sum(num_1: float,num_2: float) -> float:
     """_summary_
 
@@ -10,3 +11,33 @@ def sum(num_1: float,num_2: float) -> float:
     """
     valor_suma: float = num_1 + num_2
     return valor_suma
+
+#EJERCICIO APRENDE CON alf
+#Ejercicio 5 : Escribir una función que calcule el área de un círculo y otra que calcule el volumen de un cilindro usando la primera función.
+import math
+def area_circulo(radio:float):
+    """area_circulo: esta funcíon solicita el radio de una circunferencia y retorna el área de la circunferencia.
+
+    Args:
+        radio (float): radio de la circunferencia.
+
+    Returns:
+        area(float): área de la circunferencia  
+    """
+    area:float = radio * 3.14
+    return area
+
+def volumen_cilindro(radio:float,altura:float):
+    """volumen_cilindro: esta función solicita el radio y la altura de un cilindro y retorna el volumen de este
+
+    Args:
+        radio (float): radio de la circunferencia.
+        altura (float): altura de la circunferencia.
+
+    Returns:
+        volumen(float): volumen del cilindro
+    """
+    volumen:float = area_circulo(radio) * altura
+    return volumen 
+
+
